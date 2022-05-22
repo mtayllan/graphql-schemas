95c2240fbd7d7f6578a9f5a1e225873abd2500fc
mtayllan <messiastayllan@gmail.com>
2022-05-22 08:40:33 -0300

    Add small seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+author1 = Author.create(name: 'Messias')
+author2 = Author.create(name: 'Tayllan')
+author3 = Author.create(name: 'Anne')
+
+Post.create(title: 'My First Post', content: 'Hello World', author: author1)
+Post.create(title: 'My Second Post', content: 'Foo bar!', author: author1)
+
+Post.create(title: 'GraphQL is Awesome!', content: 'query { my { name } } ', author: author2)
+
+Post.create(title: 'Lorem ipsum', content: 'Contrary to popular belief, Lorem Ipsum is not simply random text', author: author3)
+
